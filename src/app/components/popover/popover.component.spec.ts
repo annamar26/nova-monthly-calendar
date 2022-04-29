174ec1acbfddd5458e88538b68e7745b04337fdf
Ana Marlen Aguilar <ana.aguilar@improving.com>
2022-04-29 15:13:37 -0500

    fix: test in item-dia and popover

--- src/app/components/popover/popover.component.spec.ts
+++ src/app/components/popover/popover.component.spec.ts
@@ -27,23 +27,13 @@ describe('PopoverComponent', () => {
 	it('should render the body', () => {
 		expect(fixture.nativeElement.querySelector('app-popover-body')).toBeTruthy();
 	});
-	it('"flagColorElement" prop should be undefined before view init ', () => {
-		component!.flagColorElement = undefined;
-		expect(component.flagColorElement).toBeUndefined();
-	});
-	it('"flagColorElement" prop should be define after view init ', () => {
-		const flag = component!.flagColorElement;
-		fixture.componentInstance.ngAfterViewInit();
-		fixture.detectChanges();
-
-		expect(flag).toBeTruthy();
-	});
+	
+	
   it('should change the color to the rgb color if the property exist in the input', () => {
 		component!.data.ProjectColor = 'rgb(38, 68, 220)';
 		fixture.detectChanges();
-		const flag = component!.flagColorElement;
-		fixture.componentInstance.ngAfterViewInit();
-		const styles = window.getComputedStyle(flag!);
+		const element = fixture.nativeElement.querySelector(".colorFlag")
+		const styles = window.getComputedStyle(element);
 
 		expect(styles.backgroundColor).toBe('rgb(38, 68, 220)');
 	});
