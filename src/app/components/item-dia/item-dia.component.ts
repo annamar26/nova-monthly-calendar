33596b6f51b983cc516e8f77d45033e0714c1329
Isabel Valdes <isabel.valdes@improving.com>
2022-05-01 21:32:49 -0500

    built service popover item connection

--- src/app/components/item-dia/item-dia.component.ts
+++ src/app/components/item-dia/item-dia.component.ts
@@ -1,4 +1,6 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, Input, Output, Renderer2, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, Renderer2, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+import { OnMouseOverItemService, Position } from 'src/app/services/on-mouse-over-item.service';
 
 
 interface ItemDia{
@@ -12,17 +14,36 @@ interface ItemDia{
   templateUrl: './item-dia.component.html',
   styleUrls: ['./item-dia.component.scss']
 })
-export class ItemDiaComponent  {
+export class ItemDiaComponent implements OnInit {
+  popoverState$!: Observable<boolean>;
+  position$!: Observable<Position>;
+  position!: Position;
 
   @Input() data: ItemDia = {
     AccountName: "iTexico",
     ProjectName: "Project",
     CategoryName: "Category",
     ProjectColor: "green"
-   
+  }
+
+  constructor(private onMouseOverItem: OnMouseOverItemService){}
+  
+  ngOnInit(){
+    this.popoverState$ = this.onMouseOverItem.getState$()
+    this.popoverState$.subscribe(res => console.log(res))
+    this.position$ = this.onMouseOverItem.getPosition$()
+    this.position$.subscribe(res => {
+      this.position = res;
+    })
   }
   
-  
-  
- 
+  handleMouseover(event:any):void{
+    this.onMouseOverItem.showPopover(event);
+    //this.posX = event.clientX;
+    //this.posY = event.clientY;
+  }
+
+  handleMouseout(event:any):void{
+    this.onMouseOverItem.hidePopover(event);
+  }
 }
---
+++ src/app/services/on-mouse-over-item.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+
+export interface Position {
+  positionX: number,
+  positionY: number
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OnMouseOverItemService {
+  private popoverCaller$ = new Subject<boolean>();
+  private position$ = new Subject<Position>();
+  position: Position = {
+    positionX:0,
+    positionY:0
+  };
+  state!:boolean;
+
+  showPopover(event: any):void{
+    this.popoverCaller$.next(this.state);
+    this.state = false;
+    this.position$.next(this.position)
+    let rect = event.target.getBoundingClientRect()
+    this.position = {
+      positionX: event.pageX - event.target.offsetLeft,
+      positionY: event.pageY - event.target.offsetTop
+    }
+  }
+
+  hidePopover(event: any):void{
+    this.popoverCaller$.next(this.state);
+    this.state = true;
+    this.position$.next(this.position)
+    this.position = {
+      positionX: event.clientX,
+      positionY: event.clientY
+    }
+  }
+
+  getState$():Observable<boolean>{
+    return this.popoverCaller$.asObservable();
+  }
+
+  getPosition$():Observable<Position>{
+    return this.position$.asObservable();
+  }
+}
