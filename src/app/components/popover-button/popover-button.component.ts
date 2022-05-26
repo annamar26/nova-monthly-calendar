ab2033e9536ca422cc78bf434b90f75908f84053
Isabel Valdes <isabel.valdes@improving.com>
2022-05-26 10:39:20 -0500

    add: modal title observable

--- src/app/components/popover-button/popover-button.component.ts
+++ src/app/components/popover-button/popover-button.component.ts
@@ -1,4 +1,5 @@
 import { Observable } from 'rxjs';
+import { Observable as Wobservable }  from 'windowed-observable';
 import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { ServiceDeleteModalService } from 'src/app/services/delete-modal/service-delete-modal.service';
 
@@ -13,6 +14,8 @@ export class PopoverButtonComponent {
   modalState!: boolean;
   isVisible: boolean = false;
   modalState$!: Observable<boolean>;
+  modalTitle: Wobservable = new Wobservable('modal-title');
+  modalObservable: Wobservable = new Wobservable('modal-state');
   message!: string;
 
   constructor(private ServiceDeleteModalService: ServiceDeleteModalService) {}
@@ -22,8 +25,12 @@ export class PopoverButtonComponent {
   handleClickPopoverButton(action: string) {
     if (action === 'update') {
       this.message = 'Update Activity';
+      this.modalObservable.publish(true);
+      this.modalTitle.publish(this.message);
     } else if (action === 'clone') {
       this.message = 'Clone an Activity';
+      this.modalObservable.publish(true);
+      this.modalTitle.publish(this.message);
     } else if (action === 'delete') {
       this.ServiceDeleteModalService.showModal();
          }
