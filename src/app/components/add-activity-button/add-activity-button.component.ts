ea220c6fee1dacacd8969e478f073510ef3293d5
Ana Marlen Aguilar <ana.aguilar@improving.com>
2022-06-21 17:04:57 -0500

    add observable

--- src/app/components/add-activity-button/add-activity-button.component.ts
+++ src/app/components/add-activity-button/add-activity-button.component.ts
@@ -1,5 +1,5 @@
 import { Observable } from 'windowed-observable';
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { Component, Input, OnChanges, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-add-activity-button',
@@ -7,22 +7,29 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./add-activity-button.component.scss'],
   encapsulation: ViewEncapsulation.Emulated,
 })
-export class AddActivityButtonComponent {
+export class AddActivityButtonComponent implements OnChanges{
   @Input() type: 'general' | 'day' = 'general';
+  @Input() date:Date = new Date()
   modalObservable: Observable = new Observable('modal-state');
   modalTitle: Observable = new Observable('modal-title');
+  createDate$: Observable = new Observable('create-day')
   isPublished: boolean = false;
   message: string = '';
 
   handleClick() {
     this.modalObservable.publish(true);
+    this.createDate$.publish(this.date)
     this.isPublished = true;
     if (this.type === 'general') {
       this.message = 'Create multiple activities';
     }
     if (this.type === 'day') {
       this.message = 'Create activity';
+
     }
     this.modalTitle.publish(this.message);
+  }
+  ngOnChanges(){
+
   }
 }
